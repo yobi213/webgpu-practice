2a53895e714b8713934182e0d7e893940840b832
yobi <yeop7747@gmail.com>
2023-10-01 23:31:55 +0900

    simple-chat

---
+++ simple-chat/src/gh-config.js
@@ -0,0 +1,35 @@
+export default {
+  model_list: [
+    {
+      model_url:
+        "https://huggingface.co/mlc-ai/mlc-chat-Llama-2-7b-chat-hf-q4f32_1/resolve/main",
+      local_id: "Llama-2-7b-chat-hf-q4f32_1",
+    },
+    {
+      model_url:
+        "https://huggingface.co/mlc-ai/mlc-chat-RedPajama-INCITE-Chat-3B-v1-q4f32_0/resolve/main/",
+      local_id: "RedPajama-INCITE-Chat-3B-v1-q4f32_0",
+    },
+    {
+      model_url:
+        "https://huggingface.co/yobi/RedPajama-INCITE-Chat-3B-v1-q4f32_1/resolve/main/main",
+      local_id: "RedPajama-INCITE-Chat-3B-v1-q4f32_1",
+    },
+    {
+      model_url:
+        "https://huggingface.co/yobi/llama-2-ko-7b-q4f16_1/resolve/main/",
+      local_id: "llama-2-ko-7b-q4f16_1",
+    },
+  ],
+  model_lib_map: {
+    "Llama-2-7b-chat-hf-q4f32_1":
+      "https://raw.githubusercontent.com/mlc-ai/binary-mlc-llm-libs/main/Llama-2-7b-chat-hf-q4f32_1-webgpu.wasm",
+    "RedPajama-INCITE-Chat-3B-v1-q4f32_0":
+      "https://raw.githubusercontent.com/mlc-ai/binary-mlc-llm-libs/main/RedPajama-INCITE-Chat-3B-v1-q4f32_0-webgpu-v1.wasm",
+    "RedPajama-INCITE-Chat-3B-v1-q4f32_1":
+      "https://raw.githubusercontent.com/mlc-ai/binary-mlc-llm-libs/main/RedPajama-INCITE-Chat-3B-v1-q4f32_1-webgpu.wasm",
+    "llama-2-ko-7b-q4f16_1":
+      "https://raw.githubusercontent.com/yobi213/webgpu-practice/main/llama-2-ko-7b-q4f16_1-webgpu.wasm",
+  },
+  use_web_worker: true,
+};
